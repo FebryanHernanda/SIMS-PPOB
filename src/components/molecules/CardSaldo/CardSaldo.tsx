b9d5d3081bcf4bb84cbafebecc5d3b5373c70220
Febryan Hernanda <febryanhernanda@Febryans-MacBook-Pro.local>
2025-06-06 22:08:59 +0700

    fix:[FinalCode] cardSaldo src image

--- src/components/molecules/CardSaldo/CardSaldo.tsx
+++ src/components/molecules/CardSaldo/CardSaldo.tsx
@@ -19,7 +19,7 @@ const CardSaldo = () => {
     setShowSaldo(!showSaldo);
   };
   return (
-    <div className="flex flex-col w-xl h-[200px] gap-5 p-5 rounded-2xl text-left bg-[url(/src/assets/bg-saldo.png)] bg-no-repeat bg-cover bg-right bg-clip-border text-white justify-center">
+    <div className="flex flex-col w-xl h-[200px] gap-5 p-5 rounded-2xl text-left bg-[url(/assets/bg-saldo.png)] bg-no-repeat bg-cover bg-right bg-clip-border text-white justify-center">
       <Typography variant="h4">Saldo Anda</Typography>
       <Typography variant="h1">
         {showSaldo ? `${moneyFormat(amount.balance.toString())}` : "Rp •••••••"}
